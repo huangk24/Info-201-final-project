b006ac1f115b385327524629e3279ba3326f0d71
Kai Huang <kai@D-10-19-60-187.dhcp4.washington.edu>
2018-05-25 18:03:05 -0700

    update css

--- app.R
+++ app.R
@@ -2,16 +2,18 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
-library(shiny)
 library(shinythemes)
 load("movie_data.Rda")
 source("./scripts/question1.R")
 source("./scripts/question2.R")
 
 
-ui <- navbarPage("Movie Analysis", theme = "style.css", q1_ui, q2_ui)
+#ui <- navbarPage("Movie Analysis", theme = "style.css", q1_ui, q2_ui)
 
-#ui <- navbarPage(theme = shinytheme("flatly"), tags$link(rel="stylesheet", type = "text/css", href = "style.css"), "Movie Analysis", q1_ui, q2_ui)
+ui <- navbarPage(theme = shinytheme("flatly"), 
+                 "Movie Analysis", 
+                 tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+                 q1_ui, q2_ui)
 
 server <- function(input, output, session) {
   output$q1 <- renderPlotly(q1_plot(input$color, input$rating, input$num_voted))
