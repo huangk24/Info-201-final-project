29a2cbe5374b00273d71a099a37a9263d81bfe1b
yuxuan-bill <luyxapp@163.com>
2018-05-25 17:46:51 -0700

    Add style.css

--- app.R
+++ app.R
@@ -6,14 +6,14 @@ load("movie_data.Rda")
 source("./scripts/question1.R")
 source("./scripts/question2.R")
 
-ui <- navbarPage("Movie Analysis", q1_ui, q2_ui)
+ui <- navbarPage("Movie Analysis", theme = "style.css", q1_ui, q2_ui)
 
 server <- function(input, output, session) {
   output$q1 <- renderPlotly(q1_plot(input$language, input$year))
   output$q2 <- renderPlot(q2_plot(input$genre_vector, input$year_range))
   observe({
     if (input$uncheck > 0) 
-      updateCheckboxGroupInput(session, "genre_vector", selected="")
+      updateCheckboxGroupInput(session, "genre_vector", selected = "")
   })
 }
 
