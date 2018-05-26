6c50aa07bf273178f9ad6169bc4c6fc0824b03a7
Kai Huang <kai@D-10-19-60-187.dhcp4.washington.edu>
2018-05-25 17:25:45 -0700

    update shinytheme

--- app.R
+++ app.R
@@ -2,18 +2,20 @@ library(shiny)
 library(dplyr)
 library(ggplot2)
 library(plotly)
+library(shinythemes)
 load("movie_data.Rda")
 source("./scripts/question1.R")
 source("./scripts/question2.R")
 
-ui <- navbarPage("Movie Analysis", q1_ui, q2_ui)
+ui <- navbarPage(theme = shinytheme("flatly"), "Movie Analysis", q1_ui, q2_ui)
 
 server <- function(input, output, session) {
-  output$q1 <- renderPlotly(q1_plot(input$color,input$rating, input$num_voted))
+  output$q1 <- renderPlotly(q1_plot(input$color, input$rating, input$num_voted))
   output$q2 <- renderPlot(q2_plot(input$genre_vector, input$year_range))
   observe({
-    if (input$uncheck > 0) 
-      updateCheckboxGroupInput(session, "genre_vector", selected="")
+    if (input$uncheck > 0) {
+      updateCheckboxGroupInput(session, "genre_vector", selected = "")
+    }
   })
 }
 
--- scripts/question1.R
+++ scripts/question1.R
@@ -3,26 +3,35 @@ library(plotly)
 
 load("./movie_data.Rda")
 
-q1_ui <- tabPanel("Profit & Imdb Score",
-                  tags$blockquote("TODO: what I observed in this dataset"),
-                  sidebarLayout(
-                    sidebarPanel(
-                      selectInput("color", label = "Color", 
-                                  choices = c("Color", " Black and White")),
-                      selectInput("rating", label = "Content rating", 
-                                  choices = c("G", "PG", "PG-13", "R", "Not Rated")),
-                      sliderInput("num_voted", label = "Minimum number of votes", min = 0, 
-                                  max = 50000, value = 0, step = 1000)
-                    ),
-                    mainPanel(plotlyOutput("q1"))
-                  ))
+q1_ui <- tabPanel(
+  "Profit & Imdb Score",
+  tags$blockquote("TODO: what I observed in this dataset"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("color",
+        label = "Color",
+        choices = c("Color", " Black and White")
+      ),
+      selectInput("rating",
+        label = "Content rating",
+        choices = c("G", "PG", "PG-13", "R", "Not Rated")
+      ),
+      sliderInput("num_voted",
+        label = "Minimum number of votes", min = 0,
+        max = 50000, value = 0, step = 1000
+      )
+    ),
+    mainPanel(plotlyOutput("q1"))
+  )
+)
 
 q1_plot <- function(color_selected, rating, num_voted) {
-  profit_imdb <- movie_data %>%  
-    filter(color == color_selected) %>% 
-    filter(content_rating == rating) %>% 
-    filter(num_voted_users > num_voted) %>% 
+  profit_imdb <- movie_data %>%
+    filter(color == color_selected) %>%
+    filter(content_rating == rating) %>%
+    filter(num_voted_users > num_voted) %>%
     select(gross, imdb_score)
-  p <- plot_ly(data = profit_imdb, x = ~imdb_score, y = ~gross)
+  p <- plot_ly(data = profit_imdb, x = ~ imdb_score, y = ~ gross)
   p
 }
+
